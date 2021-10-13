1c83d0646c00b6c8a78d7e5cd13432c37f01d577
Theo <trgrf1@gmail.com>
2021-10-13 11:00:49 -0400

    portfolio page routing

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,10 +4,10 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <h1>Portfolio</h1>
-      <a href="https://trgrf.github.io/MovieCharacters/"> <img src="./assests/images/Movie Characters.png" alt="HomeScreen of Movie Informer"/> </a>
-      <a href="https://trgrf.github.io/DayPlanner/"> <img src="./assests/images/DayPlanner.PNG" alt="HomeScreen of DayPlanner"/> </a>
-      <a href="https://trgrf.github.io/PassWordGenerator/"> <img src="./assests/images/PasswordGen.PNG" alt="HomeScreen of PasswordGen"/> </a>
-      <a href="https://trgrf.github.io/WeatherDashboard/"> <img src="./assests/images/WeatherDashboard.PNG" alt="HomeScreen of WeatherDashboard"/> </a>
+      <a href="https://trgrf.github.io/MovieCharacters/"> <img src="./images/Movie Characters.png" alt="HomeScreen of Movie Informer"/> </a>
+      <a href="https://trgrf.github.io/DayPlanner/"> <img src="./images/DayPlanner.PNG" alt="HomeScreen of DayPlanner"/> </a>
+      <a href="https://trgrf.github.io/PassWordGenerator/"> <img src="./images/PasswordGen.PNG" alt="HomeScreen of PasswordGen"/> </a>
+      <a href="https://trgrf.github.io/WeatherDashboard/"> <img src="./images/WeatherDashboard.PNG" alt="HomeScreen of WeatherDashboard"/> </a>
     </div>
   );
 }
