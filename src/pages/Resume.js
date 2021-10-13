bd702a43c6c0336b39ead6c189c106c9485010a5
Theo <trgrf1@gmail.com>
2021-10-13 10:56:36 -0400

    Saving resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,6 +4,7 @@ function Resume() {
     return (
       <div className="portfolio">
         <h1>Resume</h1>
+        <a href="https://docs.google.com/document/d/12FlYu4WXncZzv0sRgUoXBMhosCyih7tKgHORVRrNC20/edit?usp=sharing">Resume</a>
       </div>
     );
   }
